56ccf09af92395b1deedf21c84860647cc803af7
msplint <tk@terrifickid.net>
2023-08-18 18:42:31 -0500

    deploy

--- src/routes/[country]/[city]/[property]/[id]/+page.svelte
+++ src/routes/[country]/[city]/[property]/[id]/+page.svelte
@@ -3,15 +3,15 @@
   console.log(data);
 </script>
 
-<section class="bg-black text-white bg-cover center shadow-xl pt-24">
-  <div style="background-color: rgba(0,0,0,0.7)">
+<section class="bg-black text-white bg-cover center shadow-xl pt-32 pb-24">
+  <div>
     <div class="items-center w-full frame">
       <div>
         <img class="invert" src="/logos/stripe.svg" alt="" />
         <p class="text-3xl">
           {data.nickname}
         </p>
-        <p class="mt-4 text-2xl">
+        <p class="mt-4 text-3xl">
           {data.title}
         </p>
       </div>
@@ -19,46 +19,28 @@
   </div>
 </section>
 
-<div class="bg-black py-24">
-  <div class="frame">
-    <div
-      class="picture bg-contain bg-no-repeat bg-left bg-black"
-      style="width: 100%; background-image: url({data.picture
-        .large}); height: 50vh;"
-    />
+<section>
+  <div class="bg-black">
+    <img class="w-full" src={data.picture.large} />
   </div>
-</div>
-<section class="bg-black text-white pb-24">
-  <div class=" frame">
-    <div class="grid grid-cols-1 md:grid-cols-3">
-      <div class="mb-4">
-        <h2 class="text-lg mb-2">Address</h2>
-        <p>{data.address.city}, {data.address.country}</p>
-      </div>
+</section>
 
+<section class="bg-white text-black cframe">
+  <div class=" text-3xl">
+    <div class="grid grid-cols-2 md:grid-cols-2 gap-4">
       <div class="mb-4">
-        <h2 class="text-lg mb-2">Accommodates</h2>
+        <h2 class=" mb-2">Accommodates</h2>
         <p>{data.accommodates}</p>
       </div>
 
       <div class="mb-4">
-        <h2 class="text-lg mb-2">Bathrooms</h2>
+        <h2 class="mb-2">Bathrooms</h2>
         <p>{data.bathrooms}</p>
       </div>
 
       <div class="mb-4">
-        <h2 class="text-lg mb-2">Bedrooms</h2>
+        <h2 class=" mb-2">Bedrooms</h2>
         <p>{data.bedrooms}</p>
-      </div>
-
-      <div class="mb-4">
-        <h2 class="text-lg mb-2">Default Check-In Time</h2>
-        <p>{data.defaultCheckInTime}</p>
-      </div>
-
-      <div class="mb-4">
-        <h2 class="text-lg mb-2">Default Check-Out Time</h2>
-        <p>{data.defaultCheckOutTime}</p>
       </div>
 
       <!-- Continue displaying other properties -->
@@ -66,28 +48,29 @@
   </div>
 </section>
 
-<section class=" bg-white text-black py-24">
-  <div class="frame">
-    <p class="text-3xl pb-12">Property Description</p>
-    <div class="mb-4 text-lg" style="columns: 2; column-gap: 4rem;">
-      <p>{data.publicDescription.summary}</p>
+<div class="grid grid-cols-1 frame">
+  <section class=" bg-white text-black">
+    <div>
+      <div class="pb-24 text-lg">
+        <p>{data.publicDescription.summary}</p>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section class="grid grid-cols-4 gap-5 p-8">
-  {#each data.pictures as picture}
-    <div
-      class="picture bg-cover bg-center aspect-square"
-      style="width: 100%; background-image: url({picture.large})"
-    />
-  {/each}
-</section>
+  <section class=" col-span-2 grid grid-cols-4 gap-4">
+    {#each data.pictures as picture}
+      <div
+        class="picture bg-cover bg-center aspect-square"
+        style="width: 100%; background-image: url({picture.large})"
+      />
+    {/each}
+  </section>
+</div>
 
-<section class="bg-bound text-white py-24">
-  <div class="mb-4 frame">
+<section class="bg-white text-black">
+  <div class="mb-4 cframe">
     <h2 class="mb-2 text-3xl pb-12">Amenities</h2>
-    <ul class="list-none grid grid-cols-3 gap-8 text-2xl">
+    <ul class="list-none grid grid-cols-2 gap-8 text-3xl">
       {#each data.amenities as amenity}
         <li>{amenity}</li>
       {/each}
