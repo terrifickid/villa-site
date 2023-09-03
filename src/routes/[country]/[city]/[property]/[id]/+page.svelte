4cb594d125b193f6772f96dd4734be51644c4a68
msplint <tk@terrifickid.net>
2023-09-03 00:06:55 -0500

    deploy

--- src/routes/[country]/[city]/[property]/[id]/+page.svelte
+++ src/routes/[country]/[city]/[property]/[id]/+page.svelte
@@ -66,8 +66,8 @@
 <p class="mb-1" />
 
 <section class="bg-white text-black frame">
-  <div class="text-lg md:text-2xl lg:text-3xl">
-    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
+  <div class=" md:text-2xl lg:text-3xl">
+    <div class="grid grid-cols-3 lg:grid-cols-3 gap-4">
       <div class="mb-4">
         <p>Guests<br />{data.accommodates}</p>
       </div>
