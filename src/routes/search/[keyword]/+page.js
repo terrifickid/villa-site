f05d60eee72214faa216afe48fae9da5a6b67ec9
msplint <tk@terrifickid.net>
2023-11-07 02:05:30 -0600

    deploy

--- src/routes/search/[keyword]/+page.js
+++ src/routes/search/[keyword]/+page.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 export async function load({ params }) {
-  console.log("morg...", params);
   const apiUrl = "http://localhost:8080/keyword";
   try {
     const response = await axios.post(apiUrl, {
       search: params.keyword,
     });
-    console.log("data", response);
     return { results: response.data, keyword: params.keyword };
   } catch (error) {
     console.error(error);
