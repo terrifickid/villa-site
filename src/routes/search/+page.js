1625ab243bcda0f6768e2f291d08f0cde781ae43
msplint <tk@terrifickid.net>
2024-04-26 19:53:16 -0500

    deploy

--- src/routes/search/+page.js
+++ src/routes/search/+page.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export async function load({ url }) {
-  var searchString = url.search + "&tags=iCal";
+  var searchString = url.search;
   console.log("Searching...", searchString);
   const apiUrl = "https://vapi-le6wug7tlq-vp.a.run.app/search";
   try {
